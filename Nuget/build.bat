055a674fbb3fef1ee12fbb8bc020a49a1f92bf0e
cezarypiatek <cezarypiatek@live.com>
2015-04-23 22:48:11 +0200

    Fix build script

--- Nuget/build.bat
+++ Nuget/build.bat
@@ -16,5 +16,7 @@ if "%nuget%" == "" (
  
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Src\VanillaTransformer\VanillaTransformer.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false 
 xcopy  "Src\VanillaTransformer\bin\%config%" "Nuget\tools\" /s/h/e/k/f/c/Y
- 
+
+mkdir Build
+
 %nuget% pack "Nuget\VanillaTransformer.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
