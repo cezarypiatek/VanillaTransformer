b7ca9f3d045ed1f0de83da697b77dade170c291d
cezarypiatek <cezarypiatek@live.com>
2015-05-07 22:48:24 +0200

    Add PS command to add transformation config to csproj

--- Nuget/tools/VanillaTransformerCommands.psm1
+++ Nuget/tools/VanillaTransformerCommands.psm1
@@ -1,17 +1,33 @@
 function Add-Transformation($pattern, $values, $output){
-$project = Get-Project
-$buildProject =  @([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName))[0]
-$afterBuildTarget= $buildProject.Xml.Targets | Where-Object {$_.Name -eq "AfterBuild"}
-if($afterBuildTarget -eq $null)
-{
-	$afterBuildTarget = $buildProject.Xml.AddTarget("AfterBuild")
-	Write-Host "Build target 'AfterBuild' has been created successfully."
+	$project = Get-Project
+	$buildProject =  @([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName))[0]
+	$afterBuildTarget= $buildProject.Xml.Targets | Where-Object {$_.Name -eq "AfterBuild"}
+	if($afterBuildTarget -eq $null)
+	{
+		$afterBuildTarget = $buildProject.Xml.AddTarget("AfterBuild")
+		Write-Host "Build target 'AfterBuild' has been created successfully."
+	}
+	$task = $afterBuildTarget.AddTask("VanillaTransformerTask")
+	$task.SetParameter("PatternFile", $pattern)
+	$task.SetParameter("ValuesSource", $values)
+	$task.SetParameter("OutputPath",$output)
+	$buildProject.Save()
+	$project.Save()
+	Write-Host "Transformation has been added successfully."
 }
-$task = $afterBuildTarget.AddTask("VanillaTransformerTask")
-$task.SetParameter("PatternFile", $pattern)
-$task.SetParameter("ValuesSource", $values)
-$task.SetParameter("OutputPath",$output)
-$buildProject.Save()
-$project.Save()
-Write-Host "Transformation has been added successfully."
+
+function Add-TransformationConfig($configFilePath){
+	$project = Get-Project
+	$buildProject =  @([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName))[0]
+	$afterBuildTarget= $buildProject.Xml.Targets | Where-Object {$_.Name -eq "AfterBuild"}
+	if($afterBuildTarget -eq $null)
+	{
+		$afterBuildTarget = $buildProject.Xml.AddTarget("AfterBuild")
+		Write-Host "Build target 'AfterBuild' has been created successfully."
+	}
+	$task = $afterBuildTarget.AddTask("VanillaTransformerTask")
+	$task.SetParameter("TransformConfiguration", $configFilePath)	
+	$buildProject.Save()
+	$project.Save()
+	Write-Host "Transformation configuration has been added successfully."
 }
