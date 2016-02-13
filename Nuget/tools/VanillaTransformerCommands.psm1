03ea3e5953a97cce0607fe885625afa7417fbb77
= <cezarypiatek@live.com>
2016-02-13 10:57:08 +0100

    Add command for bootstrapping configuration transformations

--- Nuget/tools/VanillaTransformerCommands.psm1
+++ Nuget/tools/VanillaTransformerCommands.psm1
@@ -113,4 +113,169 @@ function Invoke-Transformations
     [System.IO.Directory]::SetCurrentDirectory($oldPath)
 }
 
-Export-ModuleMember -Function Add-Transformation, Add-TransformationConfig, Invoke-Transformations
+function Get-Confirmation($message){
+
+    $title = ""
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Create transformation config."
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Ignore file."
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+    $result = $host.ui.PromptForChoice($title, $message, $options, 0) 
+    switch ($result)
+    {
+        0 { $true }
+        1 { $false}
+    }
+}
+
+function Search-ItemsRecursive{
+  [CmdletBinding()]
+  param($projectItems, $filter, [switch]$recurse=$false)
+  foreach ($item in $projectItems) {
+    if($(. $filter $item))
+    {
+        $item
+    }else{
+        if(($item.ProjectItems -ne $null) -and $recurse){
+            Search-ItemsRecursive -projectItems $item.ProjectItems -filter $filter -recurse:$recurse
+        }
+    }
+  }
+}
+
+function Get-RelativePath($path, $relativeTo){
+    $path.Replace($relativeTo,"").TrimStart("\")
+}
+
+function Add-ProjectDirectoryIfNotExist($Project, $DirPath)
+{
+    $projectPath = Split-Path $Project.FileName -Parent
+    $fullPathToNewDire ="$projectPath\$DirPath"
+    if((Test-Path $fullPathToNewDire) -ne $true){
+        [void](New-Item -ItemType Directory -Force -Path  $fullPathToNewDire)
+        $outRoot = ($DirPath -split "\\")[0]
+        if([string]::IsNullOrWhiteSpace($outRoot) -ne $true)
+        {
+            [void]$Project.ProjectItems.AddFromDirectory("$projectPath\$outRoot")
+        }
+    }
+}
+
+function Create-TransformationConfigurationFile($TransformationDetails, $OutPath, [switch]$Force=$true)
+{
+    [System.XML.XMLDocument]$configDocument = if(($Force -eq $false) -and (Test-Path $OutPath)){
+        $existingConf = [xml](Get-Content $OutPath)
+        if($existingConf -eq $null)
+        {
+            New-Object System.XML.XMLDocument
+        }else{
+            $existingConf
+        }
+    } else{
+        New-Object System.XML.XMLDocument
+    } 
+
+
+    [System.XML.XMLElement]$configRoot = $configDocument.SelectSingleNode("//root")
+    if($configRoot -eq $null)
+    {
+        $configRoot = $configDocument.CreateElement("root")
+        [void]$configDocument.appendChild($configRoot)
+    }    
+
+    foreach($group in $TransformationDetails.Groups)
+    {
+        $transformationGroupNode = $configRoot.SelectSingleNode("//transformationGroup[@pattern='$($group.Pattern)']")
+        if($transformationGroupNode -eq $null)
+        {
+            $transformationGroupNode = $configDocument.CreateElement("transformationGroup")
+            [void]$configRoot.appendChild($transformationGroupNode)
+        }
+        [void]$transformationGroupNode.SetAttribute("pattern", $group.Pattern)
+
+
+        foreach($transformation in $group.Transformations)
+        {
+            $transformationNode = $transformationGroupNode.SelectSingleNode("//transformation[@values='$($transformation.Values)']")
+            if($transformationNode -eq $null)
+            {
+                $transformationNode = $configDocument.CreateElement("transformation")
+                [void]$transformationGroupNode.appendChild($transformationNode)
+            }
+            
+            [void]$transformationNode.SetAttribute("values",$transformation.Values)
+            [void]$transformationNode.SetAttribute("output", $transformation.Output)
+        }
+    }
+   
+    [void]$configDocument.Save($OutPath)
+}
+
+function Create-BoostrapConfig
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string[]]$Enviroments,
+        [string]$ConfigFilter="*.config",
+        [switch]$SearchRecurse=$false,
+        [string]$DefaultEnviroment,
+        [string]$TransformationsOut,
+        [switch]$Force
+     )
+    $project = Get-Project
+    $projectPath = Split-Path $project.FileName -Parent
+    Add-ProjectDirectoryIfNotExist -Project $project -DirPath $TransformationsOut
+
+    $emptyValuesFileContent= "<?xml version=`"1.0`" encoding=`"utf-8`" ?>`r`n<root>`r`n</root>" 
+    $transformationDetails = @{Groups=@()}
+
+    Search-ItemsRecursive -projectItems $project.ProjectItems -filter {param($item) $item.Name -like $ConfigFilter } -recurse:$SearchRecurse |% {        
+        $filePath = $_.Properties.Item("FullPath").Value
+        if(Get-Confirmation "Do you want to create config transformation for: '$($filePath)'")
+        {  
+            Write-Verbose "Create Transformation for '$($filePath)'"
+            $fileName = Split-Path $filePath -Leaf
+            $fileDirectory = Split-Path $filePath -Parent
+            $fileExtension = [System.IO.Path]::GetExtension($fileName)
+            $filePrefix =$fileName -replace $fileExtension,""
+            $patternFilePath = "$fileDirectory\$filePrefix.pattern$($fileExtension)"
+            if((Test-Path $patternFilePath) -eq $false)
+            {
+                Copy-Item -Path $filePath -Destination $patternFilePath
+                [void]$_.ProjectItems.AddFromFile($patternFilePath)
+            }else{
+                Write-Verbose "`tPattern file for '$($filePath)' already exists"
+            }
+            $transformationGroup = @{Pattern = $(Get-RelativePath $patternFilePath -relativeTo $projectPath); Transformations=@()}
+            $transformationDetails.Groups += $transformationGroup
+
+            foreach($enviroment in $Enviroments)
+            {
+              $valuesFilePath = "$fileDirectory\$filePrefix.values.$enviroment$($fileExtension)"
+              if($(Test-Path $valuesFilePath) -eq $false )
+              {
+                Out-File -FilePath  $valuesFilePath -Encoding utf8 -InputObject $emptyValuesFileContent
+                [void]$_.ProjectItems.AddFromFile($valuesFilePath)
+                Write-Verbose "`tSuccessfully created file with values for environment: '$enviroment'"
+              }else{
+                Write-Verbose "`tFile with values for environment: '$enviroment' already exists"
+              }
+              
+              $outputPath = if($enviroment -eq $DefaultEnviroment){ $filePath } else{ "$TransformationsOut\$($filePrefix)_$($enviroment)$($fileExtension)" }
+              $transformationGroup.Transformations+= @{
+                Values= $(Get-RelativePath $valuesFilePath -relativeTo $projectPath);
+                Output= $(Get-RelativePath $outputPath -relativeTo $projectPath)
+              }
+            }
+        }
+    }
+   
+    $transformationsFilePath = "$projectPath\transformations.xml"
+    Create-TransformationConfigurationFile -TransformationDetails $transformationDetails -OutPath $transformationsFilePath -Force:$Force
+    [void]$project.ProjectItems.AddFromFile($transformationsFilePath)
+    $project.Save()
+    Add-TransformationConfig "transformations.xml"
+}
+
+
+Export-ModuleMember -Function Add-Transformation, Add-TransformationConfig, Invoke-Transformations, Create-BoostrapConfig
