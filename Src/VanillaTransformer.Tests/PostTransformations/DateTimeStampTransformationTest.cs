c998fd0d791ea16632283348d3289cd7fd469d4a
Cezary Piątek <cezarypiatek@live.com>
2016-02-26 22:26:05 +0100

    Added DateTimeStampTransformation

---
+++ Src/VanillaTransformer.Tests/PostTransformations/DateTimeStampTransformationTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using NUnit.Framework;
+using VanillaTransformer.PostTransformations.XML;
+
+namespace VanillaTransformer.Tests.PostTransformations
+{
+    [TestFixture]
+    public class DateTimeStampTransformationTest
+    {
+        [Test]
+        public void should_be_able_to_add_datetime_stamp_to_transformed_text()
+        {
+            //ARRANGE
+            var transformed = new DateTimeStampTransformation();
+            const string sampleXml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<root>\r\n    <body>Test</body>\r\n</root>";
+
+            //ACT
+            var result = transformed.Execute(sampleXml);
+            
+
+            //ASSERT
+            Assert.IsTrue(Regex.IsMatch(result, @"^<!-- Generated on: (.*?) -->\s{2}.*$", RegexOptions.Singleline));
+        }
+    }
+}
