6de7e5adf2ee8726fc3ae1003e17ff2b6f162271
Cezary Piątek <cepi@ailleron.com>
2016-12-19 22:41:20 +0100

    Extract GenericPlaceholderTransformer

---
+++ Src/VanillaTransformer/Transformers/GenericPlaceholderTransformer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace VanillaTransformer.Transformers
+{
+    public abstract class GenericPlaceholderTransformer : ITransformer
+    {
+        private readonly string placeholderFormatString;
+        private readonly string placeholderPatternString;
+
+        protected GenericPlaceholderTransformer(string placeholderFormatString, string placeholderPatternString)
+        {
+            this.placeholderFormatString = placeholderFormatString;
+            this.placeholderPatternString = placeholderPatternString;
+        }
+
+        public string Transform(string configurationPattern, IDictionary<string, string> configurationValues)
+        {
+            var transformedConfiguration = ReplacePlaceholdersWithValues(configurationPattern, configurationValues);
+            ValidateTransformedText(transformedConfiguration);
+            return transformedConfiguration;
+        }
+
+        private string ReplacePlaceholdersWithValues(string configurationPattern, IDictionary<string, string> configurationValues)
+        {
+            var transformedConfiguration = configurationPattern;
+            foreach (var value in configurationValues)
+            {
+                var valuePlaceholder = string.Format(placeholderFormatString, value.Key);
+                transformedConfiguration = transformedConfiguration.Replace(valuePlaceholder, value.Value);
+            }
+            return transformedConfiguration;
+        }
+
+        private void ValidateTransformedText(string transformedText)
+        {
+            var placeholderPattern = new Regex(placeholderPatternString, RegexOptions.Multiline);
+            var matches = placeholderPattern.Matches(transformedText);
+            if (matches.Count > 0)
+            {
+                var missingValuesNames = matches.OfType<Match>()
+                    .Select(match => match.Groups[1].Value)
+                    .Distinct()
+                    .ToList();
+
+                throw new MissingValuesException(missingValuesNames);
+            }
+        }
+    }
+}
--- Src/VanillaTransformer/Transformers/HashBracketPlaceholderTransformer.cs
+++ Src/VanillaTransformer/Transformers/HashBracketPlaceholderTransformer.cs
@@ -1,42 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-namespace VanillaTransformer.Transformers
+﻿namespace VanillaTransformer.Transformers
 {
-    public class HashBracketPlaceholderTransformer : ITransformer
+    public class HashBracketPlaceholderTransformer : GenericPlaceholderTransformer
     {
-        public string Transform(string configurationPattern, IDictionary<string, string> configurationValues)
+        public HashBracketPlaceholderTransformer() : base("#[{0}]", @"\#\[(.*?)\]")
         {
-            var transformedConfiguration = ReplacePlaceholdersWithValues(configurationPattern, configurationValues);
-            ValidateTransformedText(transformedConfiguration);
-            return transformedConfiguration;
-        }
-
-        private static string ReplacePlaceholdersWithValues(string configurationPattern, IDictionary<string, string> configurationValues)
-        {
-            var transformedConfiguration = configurationPattern;
-            foreach (var value in configurationValues)
-            {
-                var valuePlaceholder = string.Format("#[{0}]", value.Key);
-                transformedConfiguration = transformedConfiguration.Replace(valuePlaceholder, value.Value);
-            }
-            return transformedConfiguration;
-        }
-
-        private static void ValidateTransformedText(string transformedText)
-        {
-            var placeholderPattern = new Regex(@"\#\[(.*?)\]", RegexOptions.Multiline);
-            var matches = placeholderPattern.Matches(transformedText);
-            if (matches.Count > 0)
-            {
-                var missingValuesNames = matches.OfType<Match>()
-                    .Select(match => match.Groups[1].Value)
-                    .Distinct()
-                    .ToList();
-
-                throw new MissingValuesException(missingValuesNames);
-            }
         }
     }
 }
