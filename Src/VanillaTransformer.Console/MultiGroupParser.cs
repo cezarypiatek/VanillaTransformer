7fe356d96c82de88078af16204866a10f9b72181
Cezary Piatek <cezarypiatek@live.com>
2018-10-03 23:14:04 +0200

    Add help and parameter groups for commandline app

---
+++ Src/VanillaTransformer.Console/MultiGroupParser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fclp;
+using Fclp.Internals;
+using VanillaTransformer.Core;
+
+namespace VanillaTransformer.Console
+{
+    internal class MultiGroupParser
+    {
+        private readonly IList<FluentCommandLineParser<InputParameters>> parsers;
+
+        public MultiGroupParser(IList<FluentCommandLineParser<InputParameters>> parsers)
+        {
+            this.parsers = parsers;
+            foreach (var parser in parsers)
+            {
+                parser.SetupHelp("?", "help")
+                    .Callback(text => PrintCompleteHelp());
+            }
+        }
+
+        private void PrintCompleteHelp()
+        {
+            if (helpWasFired)
+            {
+                return;
+            }
+            System.Console.WriteLine($"=============== VanillaTransformer HELP ===============");
+            for (int i = 0; i < this.parsers.Count; i++)
+            {
+                if (this.parsers[i] is var parser && parser.HelpOption is HelpCommandLineOption help)
+                {
+                    System.Console.WriteLine($"=============== Parameter Group {i + 1} ===============");
+                    var formattedHelp = FormattedHelp(help, parser);
+                    System.Console.WriteLine(formattedHelp);
+                }
+            }
+
+            this.helpWasFired = true;
+        }
+
+        private static string FormattedHelp(HelpCommandLineOption helpCommandLineOption, FluentCommandLineParser<InputParameters> fluentCommandLineParser)
+        {
+            var helpText = helpCommandLineOption.OptionFormatter.Format(fluentCommandLineParser.Options).Trim();
+            var helpLines = helpText.Split('\n').Select(x =>
+            {
+                var parts = x.Split(':');
+                return $"  -{parts[0].Trim()}  --{parts[1].TrimStart()}";
+            });
+            return string.Join("\n", helpLines);
+        }
+
+        private bool helpWasFired = false;
+
+        public InputParameters Parse(string[] args)
+        {
+            this.helpWasFired = false;
+            var results =  parsers.Select(x => new {parser = x, result = x.Parse(args)})
+                .OrderByDescending(x => x.result.UnMatchedOptions.Count()).ToList();
+
+            if (results.Any(x=>x.result.HelpCalled))
+            {
+                return null;
+            }
+
+            var succeeded = results.FirstOrDefault(x => x.result.HasErrors == false);
+            if (succeeded != null)
+            {
+                return succeeded.parser.Object;
+            }
+
+            var firstAlmostMatched = results.FirstOrDefault(x=>x.result.UnMatchedOptions.Count() != x.parser.Options.Count());
+            throw new Exception(firstAlmostMatched?.result.ErrorText ?? "Cannot match any parameters. Run with -? parameter to get complete list of parameters.");
+        }
+    }
+}
--- Src/VanillaTransformer.Console/Program.cs
+++ Src/VanillaTransformer.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Fclp;
 using VanillaTransformer.Core;
 
@@ -10,46 +11,80 @@ namespace VanillaTransformer.Console
         {
             try
             {
-                var parser = new FluentCommandLineParser<InputParameters>();
-                SetupParser(parser);
 
-                var result = parser.Parse(args);
-                if (result.HasErrors == false)
+                var parser = SetupParser();
+                var transformerParameters = parser.Parse(args);
+                if (transformerParameters != null)
                 {
-                    IVanillaTransformer vanillaTransformer = new Core.VanillaTransformer(parser.Object);
+                    var vanillaTransformer = new Core.VanillaTransformer(transformerParameters);
                     vanillaTransformer.LaunchTransformations();
                 }
-                else
-                  System.Console.WriteLine(result.ErrorText);
-                                        
             }
             catch (Exception e)
             {
-                System.Console.WriteLine(e);
+                System.Console.Error.WriteLine($"ERROR: {e.Message}");
             }
         }
 
-        private static void SetupParser(FluentCommandLineParser<InputParameters> fluentCommandLineParser)
+        private static MultiGroupParser SetupParser()
         {
-            fluentCommandLineParser.Setup(arg => arg.TransformConfiguration)
-                .As('t', "TransformConfiguration");
-            fluentCommandLineParser.Setup(arg => arg.PatternFile)
-                .As('p', "PatternFile");
-            fluentCommandLineParser.Setup(arg => arg.OutputPath)
-                .As('o', "OutputPath");
-            fluentCommandLineParser.Setup(arg => arg.ValuesSource)
-                .As('s', "ValuesSource");
-            fluentCommandLineParser.Setup(arg => arg.OutputArchivePath)
-                .As('a', "OutputArchivePath");
-            fluentCommandLineParser.Setup(arg => arg.ValuesProviderName)
-                .As('n', "ValuesProviderName");
-            fluentCommandLineParser.Setup(arg => arg.PlaceholderPattern)
-                .As('h', "PlaceholderPattern");
-            fluentCommandLineParser.Setup(arg => arg.ProjectRootPath)
-                .As('r', "ProjectRootPath");
+            return new MultiGroupParser(new List<FluentCommandLineParser<InputParameters>>()
+            {
+                SetupSingleFileParser(),
+                SetupConfigFileParser()
+            });
         }
 
-      
+        private static FluentCommandLineParser<InputParameters> SetupConfigFileParser()
+        {
+            var parser = new FluentCommandLineParser<InputParameters>();
+            parser.Setup(arg => arg.TransformConfiguration)
+                .As('t', "TransformConfiguration")
+                .WithDescription("Path to a file with transform configuration")
+                .Required();
+            SetupCommonOptions(parser);
+            return parser;
+        }
 
+        private static FluentCommandLineParser<InputParameters> SetupSingleFileParser()
+        {
+            var parser = new FluentCommandLineParser<InputParameters>();
+            parser.Setup(arg => arg.PatternFile)
+                .As('p', "PatternFile")
+                .WithDescription("Path to a file with configuration pattern")
+                .Required();
+
+            parser.Setup(arg => arg.OutputPath)
+                .As('o', "OutputPath")
+                .WithDescription("Path to a output file")
+                .Required();
+
+            parser.Setup(arg => arg.ValuesSource)
+                .As('s', "ValuesSource")
+                .WithDescription("Path to a file with values required by the pattern file")
+                .Required();
+
+            parser.Setup(arg => arg.OutputArchivePath)
+                .As('a', "OutputArchivePath")
+                .WithDescription("Path to output archive");
+
+            parser.Setup(arg => arg.ValuesProviderName)
+                .As('n', "ValuesProviderName")
+                .WithDescription("Name of the value provider");
+           
+            SetupCommonOptions(parser);
+            return parser;
+        }
+
+        private static void SetupCommonOptions(FluentCommandLineParser<InputParameters> parser)
+        {
+            parser.Setup(arg => arg.ProjectRootPath)
+                .As('r', "ProjectRootPath")
+                .WithDescription("If not set, the path of the executable will be taken");
+
+            parser.Setup(arg => arg.PlaceholderPattern)
+                .As('h', "PlaceholderPattern")
+                .WithDescription("String that define placeholders format. Every placeholder must contain KEY token. Example \"${KEY}\"");
+        }
     }
 }
