9b7e018697bf4e0a8bb2d62a0d65d81c53dae5a9
cezarypiatek <cezarypiatek@live.com>
2019-09-30 16:09:10 +0200

    #8: Fix MSNBuild task package

--- Nuget/createPack.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set version=1.0.0
-if not "%PackageVersion%" == "" (
-   set version=%PackageVersion%
-)
-set config="Release"
-nuget pack ..\Src\VanillaTransformer\VanillaTransformerTask.nuspec -NoPackageAnalysis -verbosity detailed -o ./ -Version %version% -p Configuration="%config%"
--- Nuget/tools/init.ps1
+++
@@ -1,3 +0,0 @@
-param($installPath, $toolsPath, $package)
-
-Import-Module (Join-Path $toolsPath VanillaTransformerCommands.psm1) -ArgumentList $installPath, $toolsPath, $package
