a5b9aa6e92acb6f4a0f6e34493ad146f2be2de9d
Cezary Piątek <cezarypiatek@live.com>
2019-12-14 12:02:56 +0100

    Add postransformation for reformating json files

---
+++ Src/VanillaTransformer.Core/PostTransformations/ReFormatJSONTransformation.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace VanillaTransformer.Core.PostTransformations
+{
+    public class ReFormatJSONTransformation : IPostTransformation
+    {
+        public string Name => "ReFormatJSON";
+        public string Execute(string configContent)
+        {
+            object parsedJson = JsonConvert.DeserializeObject(configContent);
+            return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+        }
+    }
+}
